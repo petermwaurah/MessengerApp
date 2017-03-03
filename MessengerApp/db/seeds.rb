2d6911b9bee332b7e6f276f40e517c001decf935
peter <pinchezjr@gmail.com>
2017-03-03 18:28:20 +0300

    added files

---
+++ MessengerApp/db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+
+m1 = Message.create(content:"huyo sio mwizi anauza nyama pale ngara.sasa alikutuliwa akikojoa")
+m2 = Message.create(content:"sasa yeye aliona polisi.akahepa")
+m3 = Message.create(content:"sasa kukimbishwa akaekelewa bonoko")
+m4 = Message.create(content:"si ata wewe unajua ukiekelewa bonoko huwezi jitoa")
